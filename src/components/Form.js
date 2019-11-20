d5e2760f0d1016ced23893584a629f71ec0f219d
MohammedAlMaqbli <Mohammed.AlMaqbli@gmail.com>
2019-11-19 20:41:38 -0800

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,7 +16,7 @@ class Form extends Component {
     ...DEFAULT_STATE
   }
 
-  handleSubmit() {
+  handleSubmit = event=> {
     event.preventDefault()
     document.getElementById("order-form").reset()
     this.props.addOrder(this.state)
@@ -26,10 +26,10 @@ class Form extends Component {
     })
   }
 
-  handleChange() {
+  handleChange = event=> {
     const itemType = event.target.name
     const item = event.target.value
-
+    console.log(item)
     !this.state[`${itemType}`].includes(item) ?
       this.setState({
         [itemType]: this.state[`${itemType}`].concat(item)
@@ -49,17 +49,17 @@ class Form extends Component {
         <form className="ui form" id="order-form" onSubmit={ this.handleSubmit }>
           <ProteinForm
             protein={ this.state.protein }
-            handleOnChange={ this.handleChange }
+            handleChange={ this.handleChange }
           />
 
           <FillingForm
             fillings={ this.state.fillings }
-            handleOnChange={ this.handleChange }
+            handleChange={ this.handleChange }
           />
 
           <ToppingForm
             toppings={ this.state.toppings }
-            handleOnChange={ this.handleChange }
+            handleChange={ this.handleChange }
           />
 
           <SideForm
